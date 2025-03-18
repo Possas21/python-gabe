b8956b0089d4be788284f8992b73bc3435c3c365
João Pedro Possas <joao.possas@escola.pr.gov.br>
2025-03-18 11:17:08 -0300

    Delete ex29.py

--- ex29.py
+++
@@ -1,6 +0,0 @@
-def exibir_menu():
-    print("Bem vindo ao menu de cadastro")
-    print("1 - Cadastrar")
-    print("2 - Ver cadastro")
-    print("3 - Sair")
-    print("-----------------------------")
